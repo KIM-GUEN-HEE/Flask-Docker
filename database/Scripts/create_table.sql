bb419622a2e58e64ababeb533a0d990d391686f5
KIM-GUEN-HEE <luca0879@korea.ac.kr>
2023-12-03 23:55:57 +0900

    docker build run

---
+++ database/Scripts/create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE SampleTable(
+    id VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NULL,
+    PRIMARY KEY(id)
+);
---
+++ database/Scripts/insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO SampleTable (id,title) VALUES ("김건희","Test");
+INSERT INTO SampleTable (id,title) VALUES ("홍길동","Test");
+INSERT INTO SampleTable (id,title) VALUES ("luca","Test");
